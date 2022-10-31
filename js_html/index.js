edde660115dd199b53744b724c1a5cccdcff0aaf
Solsticio97 <arnal.programacion@gmail.com>
2022-10-31 18:38:49 -0300

    agregamos js a index

--- js_html/index.js
+++ js_html/index.js
@@ -1,2 +1,34 @@
-let nombreIngresado = prompt("Ingresar su nombre");
-    alert("Bienvenido/a a PsicoAyuda "+nombreIngresado);
+//fetch the quotes from the API
+const getQuotes = async () => {
+  const apiUrl = "https://apimocha.com/frases/getAll";
+  try {
+    const response = await fetch(apiUrl);
+    quotes = (await response.json()).results;
+    //pick a random quote from the array
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    //display the quote
+    displayQuote(quote);
+
+  } catch (error) {
+    // Catch Error Here
+  }
+};
+
+//display the quote
+const displayQuote = (quote) => {
+    const quoteText = document.createElement("p");
+    var text = document.createTextNode(quote.quote);
+    quoteText.appendChild(text);
+    quoteText.classList.add("quote-text");
+    let quoteAuthor = document.createElement("p");
+    var text = document.createTextNode(quote.from ? quote.from : "Unknown");
+    quoteAuthor.appendChild(text);
+    quoteText.classList.add("quote-author");
+
+    const element = document.getElementById("quote");
+    element.appendChild(quoteText);
+    element.appendChild(quoteAuthor);
+};
+
+// On Load
+getQuotes();
